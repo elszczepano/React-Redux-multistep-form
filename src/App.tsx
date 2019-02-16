f33b0bca44a2197f21daad69067ca1ebceab1b5f
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-16 18:40:06 +0100

    Change spaces to tabs .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
 }
 
 export default App;
