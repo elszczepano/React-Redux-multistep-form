56a2a43fb021b2fcc36b9862542cfd524448c123
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-17 18:39:55 +0100

    Implement react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StartScreen from './Containers/StartScreen';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-            </div>
-        );
-    }
+	render() {
+		return (
+			<Router>
+				<Route exact path="/" component={StartScreen} />
+			</Router>
+		);
+	}
 }
 
 export default App;
