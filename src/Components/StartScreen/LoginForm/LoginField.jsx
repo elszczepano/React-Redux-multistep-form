79f974dfb45f6280736d00c593b00c93c8e8dfb4
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-21 18:52:42 +0100

    Add hover styles for login input.

--- src/Components/StartScreen/LoginForm/LoginField.jsx
+++ src/Components/StartScreen/LoginForm/LoginField.jsx
@@ -7,14 +7,22 @@ const Input = styled.input`
 	border-radius: 10px;
 	border: none;
 	font-size: 20px;
+	min-height: 23px;
 	outline: none;
 	padding: 1rem .7rem; 
 	color: #707070;
 	font-weight: 300;
-	&::-webkit-input-placeholder {
+	transition: .3s all;
+	::-webkit-input-placeholder {
     	color: #C3C3C4;
     	font-weight: 300;
     	font-family: 'Lato', sans-serif;
+  	}
+  	:focus {
+  		font-size: 19px;
+  	}
+  	:last-of-type {
+  		margin-bottom: 3rem;
   	}
 `;
 
