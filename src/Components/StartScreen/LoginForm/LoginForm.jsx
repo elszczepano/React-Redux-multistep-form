f827e587b9d2183945e64d84f29667348d37d4a9
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-21 18:53:57 +0100

    Add logo above the login form.

--- src/Components/StartScreen/LoginForm/LoginForm.jsx
+++ src/Components/StartScreen/LoginForm/LoginForm.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import LoginField from './LoginField';
 import LoginButton from './LoginButton';
-import styled from 'styled-components';
+import LoginLogo from './LoginLogo';
 
 const Container = styled.form`
 	display: grid;
@@ -10,13 +11,16 @@ const Container = styled.form`
 	padding: 1.5rem;
 `;
 
+
 class LoginForm extends Component {
 	render() {
 		return (
 			<Container>
+				<LoginLogo/>
 				<LoginField type="email" placeholder="Email" id="email"/>
 				<LoginField type="password" placeholder="Password" id="password"/>
-				<LoginButton backgroundColor="" backgroundColorHover="#1C4E7C" type="submit" text="Sign in"/>
+				<LoginButton backgroundColor="#2E3F8D" backgroundColorHover="#1C4E7C" type="submit" text="Sign in"/>
+				<LoginButton backgroundColor="#226D4F" backgroundColorHover="#115C3E" type="button" text="Create new account"/>
 			</Container>
 		);
 	}
