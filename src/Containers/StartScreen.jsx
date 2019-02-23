20d3e4301bdc30cbc4be0c28e5f9bfab17bdb1ba
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-23 18:09:40 +0100

    Use logo component.

--- src/Containers/StartScreen.jsx
+++ src/Containers/StartScreen.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LoginForm from '../Components/StartScreen/LoginForm/LoginForm';
 import StartScreenFooter from '../Components/StartScreen/StartScreenFooter';
+import StartScreenLogo from '../Components/StartScreen/StartScreenLogo';
 
 class StartScreen extends Component {
 	render() {
 		return (
 			<>
+				<StartScreenLogo/>
 				<LoginForm/>
 				<StartScreenFooter/>
 			</>
