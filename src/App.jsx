98b31bb18ed2146f2f07cbf5cf70621dd2829b39
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-24 18:38:16 +0100

    Install Font Awesome.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheck, faUser, faFileAlt } from '@fortawesome/free-solid-svg-icons';
+
 import StartScreen from './Containers/StartScreen';
+
+library.add(faCheck, faUser, faFileAlt);
 
 class App extends Component {
 	render() {
