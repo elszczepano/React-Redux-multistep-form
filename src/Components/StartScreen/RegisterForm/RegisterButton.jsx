939ff6f6cc54d10d6c0a833102de6b9071a9a3fe
Dominik <szczepaniak.dominik98@gmail.com>
2019-03-20 19:47:17 +0100

    Create register form basic elements: Input and Button.

---
+++ src/Components/StartScreen/RegisterForm/RegisterButton.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Button = styled.button`
+	background-color: ${({theme}) => theme.colors.primaryBlue};
+	border-radius: 10px;
+	border: none;
+	font-size: 18px;
+	outline: none;
+	padding: 1rem .7rem; 
+	color: ${({theme}) => theme.colors.white};
+	font-weight: ${({theme}) => theme.font.thin};
+	cursor: pointer;
+	transition: all .3s; 
+	box-shadow: 0px 3px 15px rgba(0,0,0,0.2);
+	:hover {
+		transform: scale(1.03);
+		background-color: ${({theme}) => theme.colors.primaryDarkBlue};
+	}
+`;
+
+const RegisterButton = ({step}) => {
+	return (
+		<Button>{step === 3 ? 'Register' : 'Next'}</Button>
+	);
+};
+
+RegisterButton.propTypes = {
+	step: PropTypes.number.isRequired
+};
+
+export default RegisterButton;
---
+++ src/Components/StartScreen/RegisterForm/RegisterField.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Input = styled.input`
+	background-color: ${({theme}) => theme.colors.lightGrey};
+	border-radius: 10px;
+	border: none;
+	font-size: 20px;
+	min-height: 23px;
+	outline: none;
+	padding: 1rem .7rem; 
+	color: ${({theme}) => theme.colors.darkGrey};
+	font-weight: ${({theme}) => theme.font.thin};
+	transition: .3s all;
+	::-webkit-input-placeholder {
+    	color: ${({theme}) => theme.colors.grey};
+    	font-weight: ${({theme}) => theme.font.thin};
+    	font-family: 'Lato', sans-serif;
+  	}
+  	:focus {
+  		font-size: 18px;
+  	}
+  	:last-of-type {
+  		margin-bottom: 2rem;
+  	}
+`;
+
+class RegisterField extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		};
+	}
+	handleChange = (e) => {
+		this.setState({
+			value: e.target.value
+		});
+	};
+	render() {
+		const {name, type, placeholder, id} = this.props;
+		return (
+			<Input onChange={this.handleChange} name={name} id={id} type={type} placeholder={placeholder}/>
+		);
+	}
+}
+
+RegisterField.propTypes = {
+	type: PropTypes.string.isRequired,
+	placeholder: PropTypes.string.isRequired,
+	id: PropTypes.string.isRequired,
+};
+
+export default RegisterField;
