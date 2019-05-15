19833a09fa36f4e42a1b0d503c744eb9ac5c338c
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-05-15 19:32:01 +0200

    Remove EDIT_REGISTER_DETAIL action.

--- src/actions/registerForm.js
+++ src/actions/registerForm.js
@@ -2,10 +2,6 @@ export const addRegisterDetail = detail => ({
 	type: 'ADD_REGISTER_DETAIL',
 	detail
 });
-export const editRegisterDetail = detail => ({
-	type: 'EDIT_REGISTER_DETAIL',
-	detail
-});
 export const clearRegisterForm = form => ({
 	type: 'CLEAR_REGISTER_FORM',
 	form
