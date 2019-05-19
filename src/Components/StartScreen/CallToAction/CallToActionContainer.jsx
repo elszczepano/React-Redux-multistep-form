bffb6559a1f06975cc594e032f6f7ac5f7b3dcfd
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-05-19 14:46:30 +0200

    Install Redux devtools.

---
+++ src/Components/StartScreen/CallToAction/CallToActionContainer.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CallToActionContainer extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+export default CallToActionContainer;
