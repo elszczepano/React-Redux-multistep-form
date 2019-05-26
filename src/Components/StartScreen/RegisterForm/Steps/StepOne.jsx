f2b6b714b99cc83364cc5a7455746ed02c52633a
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-05-26 19:38:03 +0200

    Add validation for the first step.

--- src/Components/StartScreen/RegisterForm/Steps/StepOne.jsx
+++ src/Components/StartScreen/RegisterForm/Steps/StepOne.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import RegisterField from '../RegisterField';
 import RegisterButton from '../RegisterButton';
 import ErrorField from '../../ErrorField';
@@ -9,13 +10,13 @@ const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
-	height: 21rem;
+	min-height: 18rem;
 	margin-top: 3rem;
 `;
 const FieldsContainer = styled.div`
 	display: grid;
 	grid-template-rows: 1fr;
-	grid-row-gap: 2rem;
+	grid-row-gap: 20px;
 `;
 
 class StepOne extends Component {
@@ -26,18 +27,80 @@ class StepOne extends Component {
 			name: '',
 			email: '',
 			birthDate: '',
-			errors: {
-				name: '',
-				email: '',
-				birthDate: ''
-			}
+			errors: {}
 		};
 	}
 
 	verifyStepOne = event => {
 		event.preventDefault();
+		const isNameValid = this.verifyName();
+		const isEmailValid = this.verifyEmail();
+		const isBirthDateValid = this.verifyBirthDate();
 
+		if(isNameValid && isEmailValid && isBirthDateValid) {
+			this.props.incrementStep();
+		}
 
+	};
+
+	verifyName = () => {
+		if(this.state.name === '') {
+			this.setState({
+				errors: Object.assign(this.state.errors, {name: 'This field cannot be empty.'})
+			});
+			return false;
+		}
+
+		this.setState({
+			errors: Object.assign(this.state.errors, {name: ''})
+		});
+
+		return true;
+	};
+
+	verifyEmail = () => {
+		const EMAIL_REGEX = new RegExp('^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}');
+
+		if(this.state.email === '') {
+			this.setState({
+				errors: Object.assign(this.state.errors, {email: 'This field cannot be empty.'})
+			});
+			return false;
+		}
+		console.log(EMAIL_REGEX.test(this.state.email));
+		if(!EMAIL_REGEX.test(this.state.email)) {
+			this.setState({
+				errors: Object.assign(this.state.errors, {email: 'This is not valid email address.'})
+			});
+			return false;
+		}
+
+		this.setState({
+			errors: Object.assign(this.state.errors, {email: ''})
+		});
+
+		return true;
+	};
+
+	verifyBirthDate = () => {
+		if(this.state.birthDate === '') {
+			this.setState({
+				errors: Object.assign(this.state.errors, {birthDate: 'This field cannot be empty.'})
+			});
+			return false;
+		}
+		if(moment().diff(this.state.birthDate) < 0) {
+			this.setState({
+				errors: Object.assign(this.state.errors, {birthDate: 'Invalid date.'})
+			});
+			return false;
+		}
+
+		this.setState({
+			errors: Object.assign(this.state.errors, {birthDate: ''})
+		});
+
+		return true;
 	};
 
 	handleChange = event => {
