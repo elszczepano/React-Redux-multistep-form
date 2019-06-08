9f1ddb77385bccc34fae6ba2c8f589a120c1731b
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-06-08 20:43:43 +0200

    Simplify service worker.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,15 +1,3 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
-
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read http://bit.ly/CRA-PWA
-
 const isLocalhost = Boolean(
 	window.location.hostname === 'localhost' ||
 	// [::1] is the IPv6 localhost address.
@@ -22,12 +10,8 @@ const isLocalhost = Boolean(
 
 export function register(config) {
 	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-		// The URL constructor is available in all browsers that support SW.
 		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
 		if (publicUrl.origin !== window.location.origin) {
-			// Our service worker won't work if PUBLIC_URL is on a different origin
-			// from what our page is served on. This might happen if a CDN is used to
-			// serve assets; see https://github.com/facebook/create-react-app/issues/2374
 			return;
 		}
 
@@ -35,11 +19,8 @@ export function register(config) {
 			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
 			if (isLocalhost) {
-				// This is running on localhost. Let's check if a service worker still exists or not.
 				checkValidServiceWorker(swUrl, config);
 
-				// Add some additional logging to localhost, pointing developers to the
-				// service worker/PWA documentation.
 				navigator.serviceWorker.ready.then(() => {
 					console.log(
 						'This web app is being served cache-first by a service ' +
@@ -47,7 +28,6 @@ export function register(config) {
 					);
 				});
 			} else {
-				// Is not localhost. Just register service worker
 				registerValidSW(swUrl, config);
 			}
 		});
