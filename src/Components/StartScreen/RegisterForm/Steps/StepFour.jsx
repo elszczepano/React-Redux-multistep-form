331da5d8b01358117a4323c2039efefd37376e43
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-06-21 17:02:43 +0200

    Prepare final step.

--- src/Components/StartScreen/RegisterForm/Steps/StepFour.jsx
+++ src/Components/StartScreen/RegisterForm/Steps/StepFour.jsx
@@ -1,17 +1,49 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 25rem;
+	margin-top: 1rem;
+`;
+
+const Icon = styled(FontAwesomeIcon)`
+	width: 100% !important;
+    font-size: 140px;
+    margin-top: 40px;
+   	margin-bottom: 70px;
+	color: ${({theme}) => theme.colors.grey};
+`;
+
+const Summary = styled.p`
+	text-align: center;
+`;
+
+const Bold = styled.span`
+	font-weight: ${({theme}) => theme.font.bold};
+	color: ${({theme}) => theme.color.primaryBlue};
+`;
 
 class StepFour extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			step: 4
+			step: 4,
+			name: ''
 		};
 	}
 	render() {
 		return (
-			<div>
-
-			</div>
+			<Container>
+				<Icon icon='laugh-beam' />
+				<Summary>
+					Registration process finished!<br />
+					<Bold>{this.state.name}</Bold> welcome to <Bold>FindMates</Bold> community!
+				</Summary>
+			</Container>
 		);
 	}
 }
